98eac779d6af6f4fce2a22220a531ca431459920
GU1LL0T1NE <code_kwen@outlook.com>
2025-07-28 22:36:24 -0500

    Impliment Container, InputGroup, and FormControl into App.jsx

--- jammming/src/App.jsx
+++ jammming/src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap';
 
 function App() {
-  
+  const [searchInput, setSearchInput] = useState("");
   return (
     <>
+      <Container>
+        <InputGroup className='mb-3' size='lg'>
+          <FormControl
+          placeholder='Search for songs'
+          type='input'
+          onKeyDown={event =>  {
+            if(event.key == "Enter")  {
+              console.log("input confirmed")
+            }
+          }}
+          onChange={event =>  {
+            setSearchInput(event.target.value)
+          }} 
+          />
+          <Button onClick={ ()  => {
+            console.log('Button pressed')
+          }}>
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
       
     </>
   )
