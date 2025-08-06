8a2a51aabfe7148c318e8ef4d3b838fcddc7066a
GU1LL0T1NE <code_kwen@outlook.com>
2025-08-05 22:35:24 -0500

    Move spotify auth code to script.js from App. Change title to "Jammming"

---
+++ jammming/src/script.js
@@ -0,0 +1,79 @@
+const CLIENTID = "61f0983a6f984e34aca1471ba0e3eca7";
+const CLIENTSECRET = "b980fb15a5d5421c95fdac86eb42692b";
+
+// Code Verifier
+const generateRandomString = (length) => {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+const codeVerifier  = generateRandomString(64);
+
+// Code Challenge
+const sha256 = async (plain) => {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(plain)
+  return window.crypto.subtle.digest('SHA-256', data)
+}
+
+const base64encode = (input) => {
+  return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+const hashed = await sha256(codeVerifier)
+const codeChallenge = base64encode(hashed);
+
+// Requesting user auth
+const authUrl = new URL("https://accounts.spotify.com/authorize");
+const scope = ["playlist-read-private", "playlist-read-collaborative", "playlist-modify-private", "playlist-modify-public"];
+const redirectUri = 'http://127.0.0.1:8000/callback';
+
+// Sets the generated code verifier into local storage
+window.localStorage.setItem('code_verifier', codeVerifier);
+
+const params =  {
+  response_type: 'code',
+  client_id: CLIENTID,
+  scope: scope,
+  code_challenge_method: 'S256',
+  code_challenge: codeChallenge,
+  redirect_uri: redirectUri,
+}
+
+authUrl.search = new URLSearchParams(params).toString();
+window.location.href = authUrl.toString();
+
+// Parsing the URL for code parameter
+const urlParams = new URLSearchParams(window.location.search);
+let code = urlParams.get('code');
+
+// Requesting access token 
+
+const getToken = async code => {
+
+  // stored in the previous step
+  const codeVerifier = localStorage.getItem('code_verifier');
+
+  const url = "https://accounts.spotify.com/api/token";
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      client_id: CLIENTID,
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: redirectUri,
+      code_verifier: codeVerifier,
+    }),
+  }
+
+  const body = await fetch(url, payload);
+  const response = await body.json();
+
+  localStorage.setItem('access_token', response.access_token);
+}
